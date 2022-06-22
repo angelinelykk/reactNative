2a6c9f11dc70f7610da7f3f74f7e1f4778976c1f
angelinelykk <angelinelyk@berkeley.edu>
2022-06-22 19:39:54 -0400

    mvp complete

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -15,10 +15,21 @@ export default function GameScreen() {
   const [total, setTotalQuestions] = useState(0);
   // State for the timer is handled for you.
   const [timeLeft, setTimeLeft] = useState(10);
+  const [selectedName, setSelectedName] = useState("")
 
   const [gcorrect, setCorrectMember] = useState(names[Math.floor(Math.random() * names.length)])
   const [gcorrectName, setCorrectName] = useState(nameToPic[gcorrect][0])
   const [gcorrectImage, setCorrectImage] = useState(nameToPic[gcorrect][1])
+  let no = [gcorrectName];
+    while (no.length < 4) {
+      let wrong = names[Math.floor(Math.random() * names.length)];
+      let wrongName = nameToPic[wrong][0];
+      if (!no.includes(wrongName)) {
+        no.push(wrongName);
+      }
+    }
+  no = shuffle(no);
+  const [gnameOptions, setNameOptions] = useState(no)
 
   // Called by the timer every 10 seconds
   const countDown = () => {
@@ -35,7 +46,11 @@ export default function GameScreen() {
 
   // This is used in the useEffect(...) hook bound on a specific STATE variable.
   // It updates state to present a new member & name options.
+
   const getNextRound = () => {
+    if (selectedName == gcorrectName) {
+      setCorrectScore(correctScore+1);
+    }
     // Fetches the next member name to guess.
     let correct = names[Math.floor(Math.random() * names.length)];
     let correctName = nameToPic[correct][0];
@@ -55,6 +70,7 @@ export default function GameScreen() {
     setCorrectImage(correctImage);
     setCorrectName(correctName);
     setCorrectMember(correct);
+    setNameOptions(nameOptions);
 
     setTimeLeft(10);
   };
@@ -102,6 +118,10 @@ export default function GameScreen() {
 
   const timeRemainingStr = (timeLeft / 1000).toFixed(2);
 
+  const pressedName = (name) => {
+    setSelectedName(name);
+    setTotalQuestions(total + 1);
+  }
   // Style & return the view.
   return (
     <View style={styles.container}>
@@ -115,9 +135,39 @@ export default function GameScreen() {
             style={styles.imageMember}
         />
       </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => 
+          pressedName(gnameOptions[0])
+        }
+      >
+        <Text style={styles.buttonText}>{gnameOptions[0]}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => pressedName(gnameOptions[1])}
+      >
+        <Text style={styles.buttonText}>{gnameOptions[1]}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => pressedName(gnameOptions[2])}
+      >
+        <Text style={styles.buttonText}>{gnameOptions[2]}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => pressedName(gnameOptions[3])}
+      >
+        <Text style={styles.buttonText}>{gnameOptions[3]}</Text>
+      </TouchableOpacity>
       <Text 
         style={styles.scoreText}
-        >{gcorrectName}
+        >{total}
+      </Text>
+      <Text 
+        style={styles.scoreText}
+        >{correctScore}
       </Text>
     </View>
   );
